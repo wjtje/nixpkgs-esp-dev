f9b1e211262a4cc9c1a265b227def56ef01c2d56
dvdvgt <40773635+dvdvgt@users.noreply.github.com>
2026-04-30 14:56:08 +0200

    remove unsupported/deprecated targets from examples and tests

--- pkgs/esp-idf/default.nix
+++ pkgs/esp-idf/default.nix
@@ -207,16 +207,6 @@ EOF
   buildExample = callPackage ./build-example.nix { inherit esp-idf; };
   targets = [
     "esp32"
-    "esp32c2"
-    "esp32c3"
-    "esp32s2"
-    "esp32s3"
-    "esp32c5"
-    "esp32c6"
-    "esp32h2"
-    "esp32p4"
-    "esp32c61"
-    "esp32h21"
   ];
   in
 esp-idf // {
